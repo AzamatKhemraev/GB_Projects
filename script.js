e7cbbafbdf32f53f8c3deb19d769d4926f399632
AzamatKhemraev <80476671+AzamatKhemraev@users.noreply.github.com>
2021-06-20 01:07:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,81 @@
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+// const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+const API_URL = 'http://localhost:3000'
+
+Vue.component('products-list', {
+  props: ['products'],
+  template: /*HTML*/ `
+    <div class="products-list">
+      <product-item v-for="product in products" :productsList="product"></product-item>
+    </div>
+  `,
+})
+
+Vue.component('product-item', {
+  data() {
+    return {}
+  },
+
+  props: ['productsList'],
+
+  template: /* HTML */ `
+    <div class="products-item">
+      <h3>{{productsList.product_name}}</h3>
+      <p>{{productsList.price}}</p>
+      <button class="add__btn" @click="addToCart"><i class="far fa-plus-square"></i></button>
+    </div>
+  `,
+
+  methods: {
+    async addToCart() {
+      const response = await fetch(`${API_URL}/addToCart`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(this.productsList),
+      })
+    },
+  },
+})
+
+Vue.component('cart-list', {
+  props: ['cart'],
+  template: /*HTML*/ `
+    <div class="cart-list">
+      <cart-item v-for="product in cart" :cartList="product"></cart-item>
+    </div>
+  `,
+})
+
+Vue.component('cart-item', {
+  data() {
+    return {}
+  },
+
+  props: ['cartList'],
+
+  template: /* HTML */ `
+    <div class="cart-item">
+      <h3>{{cartList.product_name}}(<span class="quantity">{{cartList.quantity}}</span>)</h3>
+      <p>{{cartList.price}}</p>
+      <button class="add__btn" @click="removeProduct"><i class="far fa-trash-alt"></i></button>
+    </div>
+  `,
+
+  methods: {
+    async removeProduct() {
+      const response = await fetch(`${API_URL}/removeFromCart`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(this.cartList),
+      })
+    },
+  },
+})
 
 Vue.component('search', {
   data() {
@@ -42,16 +119,6 @@ Vue.component('cart', {
   },
 })
 
-Vue.component('cart-body', {
-  data() {
-    return {}
-  },
-
-  props: ['show'],
-
-  template: /* HTML */ ` <div class="cart-body" v-show="show"></div> `,
-})
-
 Vue.component('error', {
   props: ['is-error'],
   template: `<h1 v-show="isError">Не удалось выполнить запрос к серверу</h1>`,
@@ -62,15 +129,14 @@ const app = new Vue({
   data: {
     products: [],
     filteredProducts: [],
-    // cartList: [],
-    // searchLine: '',
-    isVisibleCart: false,
+    cart: [],
+    isVisibleCart: true,
     isError: false,
   },
 
   methods: {
     async getProducts() {
-      const responce = await fetch(`${API_URL}/catalogData.json`)
+      const responce = await fetch(`${API_URL}/catalog`)
       if (responce.ok) {
         const catalogItems = await responce.json()
         this.products = catalogItems
@@ -78,6 +144,14 @@ const app = new Vue({
       } else {
         this.isError = true
         alert('Ошибка при соединении с сервером')
+      }
+    },
+
+    async getCart() {
+      const responce = await fetch(`${API_URL}/cart`)
+      if (responce.ok) {
+        const cartJSON = await responce.json()
+        this.cart = cartJSON
       }
     },
 
@@ -89,8 +163,8 @@ const app = new Vue({
       }
     },
 
-    getCartList(cartList) {
-      if (cartList.length == 0) {
+    getCartList(cart) {
+      if (cart.length == 0) {
         return alert('Корзина пуста')
       }
     },
@@ -98,5 +172,6 @@ const app = new Vue({
 
   async mounted() {
     await this.getProducts()
+    await this.getCart()
   },
 })
